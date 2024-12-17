a6a515fd955e745bd98437a96d97ba3dd651eb71
Eugene Molotov <molotov@it-projects.info>
2024-12-17 17:28:36 +0500

    Поправил rounding для одной из единиц измерения

--- fixes_v14.sql
+++ fixes_v14.sql
@@ -5,3 +5,6 @@ update product_template set active = true where id = 3492;
 update product_product set active = true where product_tmpl_id = 3492;
 -- incorrect working coupon program with expired items
 update coupon_program set active = false where id = 8;
+
+-- set rounding as "Product Unit of Measure"
+update uom_uom set rounding = 0.001 where id = 69;
