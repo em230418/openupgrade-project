6e093adf4791af3bbfd317e219af22310e4025d4
Eugene Molotov <molotov@it-projects.info>
2025-07-08 11:13:23 +0500

    Подготовили миграцию к 12.0

--- upload11.sh
+++ upload11.sh
@@ -12,7 +12,7 @@ echo "uploading 11 finished `date`" >> log.txt
 
 echo "removing unused moduls `date`" >> log.txt
 
-echo "env['ir.module.module'].search([('state', '=', 'installed'), ('name', 'in', ['add_remove_follower_omax', 'automatic_backup', 'ehcs_signup_captcha'])]).button_immediate_uninstall()" | docker compose run --rm -T odoo11 odoo shell -d ${PGDATABASE}
+echo "env['ir.module.module'].search([('state', '=', 'installed'), ('name', 'in', ['add_remove_follower_omax', 'automatic_backup', 'ehcs_signup_captcha', 'iap', 'account_asset', 'account_budget'])]).button_immediate_uninstall()" | docker compose run --rm -T odoo11 odoo shell -d ${PGDATABASE}
 
 echo "uploading fixes `date`" >> log.txt
 
