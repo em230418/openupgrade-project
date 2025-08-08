1ecc5f286d59e3197e3053e1c396b8e53dd58c10
Eugene Molotov <molotov@it-projects.info>
2025-08-08 14:36:25 +0500

    Исправление при миграции на 15.0

--- migrate15.sh
+++ migrate15.sh
@@ -5,6 +5,8 @@ set -xe
 
 echo "migrate to 15 started `date`" >> log.txt
 
+cat prefixes_v15.py | docker compose run --rm -T odoo15 odoo shell -d ${PGDATABASE}
+
 docker compose run --rm odoo15 odoo -d ${PGDATABASE} -u all -c /etc/odoo/odoo.conf --stop-after-init --load=base,web,openupgrade_framework
 
 echo "migrate to 15 ended  `date`" >> log.txt
