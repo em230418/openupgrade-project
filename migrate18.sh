1f974b7b29c93d2d212338504e94c541d643277d
Eugene Molotov <molotov@it-projects.info>
2025-10-07 16:24:56 +0500

    Поправка миграционных скриптов

--- migrate18.sh
+++ migrate18.sh
@@ -7,7 +7,11 @@ echo "migrate to 18 started `date`" >> log.txt
 
 docker compose run --rm odoo18 odoo -d ${PGDATABASE} -u all -c /etc/odoo/odoo.conf --stop-after-init --load=base,web,openupgrade_framework
 
-#cat postfixes_v18.py | docker compose run --rm -T odoo18 odoo shell -d ${PGDATABASE}
+echo "env['ir.module.module'].search([('state', '=', 'installed'), ('name', 'in', ['om_account_budget'])]).button_immediate_uninstall()" | docker compose run --rm -T odoo18 odoo shell -d ${PGDATABASE}
+
+echo "env['ir.module.module'].search([('name', 'in', ['crm_lead_to_task'])]).button_immediate_install()" | docker compose run --rm -T odoo18 odoo shell -d ${PGDATABASE}
+
+cat postfixes_v18.py | docker compose run --rm -T odoo18 odoo shell -d ${PGDATABASE}
 
 # upload fixes for migration
 cat fixes_v18.sql | docker compose run --rm -T -e PGPASSWORD=${PGPASSWORD} db psql -h db -p 5432 ${PGDATABASE} odoo
--- postfixes_v18.py
+++ postfixes_v18.py
@@ -1,12 +1,9 @@
 def main(env):
-    env["ir.model.data"].search([
-        ("module", "=", "website"),
-        ("name", "=", "header_call_to_action"),
-    ]).write({
-        "res_id": 3691,
-    })
+    for view_id, ext_id in env['ir.ui.view'].search([('key', '=', 'website.header_call_to_action_large')]).get_external_id().items():
+        if not ext_id:
+            REMOVE_ID = view_id
+            break
 
-    REMOVE_ID = 3265
     env["ir.ui.view"].search([("inherit_id.inherit_id", "=", REMOVE_ID)]).unlink()
     env["ir.ui.view"].search([("inherit_id", "=", REMOVE_ID)]).unlink()
     env["ir.ui.view"].search([("id", "=", REMOVE_ID)]).unlink()
